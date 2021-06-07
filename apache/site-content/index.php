c21e78b2402175ad51c295aac65bc1a3e00ec88b
arratete <arratepayo@gmail.com>
2021-06-07 15:59:11 +0200

    initial commit

---
+++ apache/site-content/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Prueba de PHP</title>
+    </head>
+    <body>
+
+        <h1>My first PHP page</h1>
+        <?php echo "Hello World"; ?>
+
+    </body>
+</html>
